c77d8d63a40e31dc5f80ff440fe148dac0dcad25
grite123454321 <grite123454321@gmail.com>
2025-08-02 23:22:01 -0500

    Navbar Creation

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="images/icon.png" type="image/png">
+    <title>Nom.io | The Perfect Lunch Solution For Schools</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link href="index.css" rel="stylesheet">
 </head>
-<body>
-    <?php
-    echo "Hello World(1)";
-    ?>
-</body>
+  <body>
+    <div class="container">
+        <div class="navigation navbar align-content-center justify-content-between border p-2 mt-3">   
+            <div >
+                <a href="#" class="nav-logo d-flex ">
+                    <img src="images/icon.png" style="width: 50px; height: 50px;" class="mt-1" alt="Logo Icon">
+                    <h2 class="mt-1 ms-0 nav-logo-text">Nom.io</h2>
+                </a>
+            </div>
+            <div class="auth-buttons mt-1 me-2">
+                <button class="me-2"><strong>Login</strong></button>
+                <button><strong>Sign Up</strong></button>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+  </body>
 </html>
