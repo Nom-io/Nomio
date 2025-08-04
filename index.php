31ab4d0fc1c648b32868a7d703c47c7881d9f877
grite123454321 <grite123454321@gmail.com>
2025-08-04 16:41:03 -0500

    dev-tiger final boss

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 <body>
 
-    <!-- NAVBAR tiger1 -->
+    <!-- NAVBAR tiger final -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
         <div class="container">
             <div class="d-flex logo align-content-center">
