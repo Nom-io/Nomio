c1c1513580d00410c9bb4a51f7309f3e0de3ec1c
awesome110 <mukunda.bharadwaj.2009@gmail.com>
2025-08-04 16:34:44 -0500

    dev-arya 2

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 <body>
 
-    <!-- NAVBAR2 -->
+    <!-- NAVBAR arya -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
         <div class="container">
             <div class="d-flex logo align-content-center">
