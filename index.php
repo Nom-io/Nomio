899d41f08cef815f73103df003be23da3cb3af5f
awesome110 <mukunda.bharadwaj.2009@gmail.com>
2025-08-04 16:47:05 -0500

    dev-arya final boss

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 <body>
 
-    <!-- NAVBAR arya final -->
+    <!-- NAVBAR arya final boss-->
     <nav class="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
         <div class="container">
             <div class="d-flex logo align-content-center">
